c3c9289fc30c25f51826d9aa7bdea135644c4429
ahmadasnawi <ahmadaries899@gmail.com>
2024-07-28 12:13:29 +0700

    first commit

---
+++ tugas.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <section>
+        <div id="container">
+            <div class="baris-atas">
+                <div class="loggo">
+                    <h1>Boggo</h1>
+                </div>
+                <div class="link">
+                    <ul>
+                        <li>Home</li>
+                        <li>Man</li>
+                        <li>Woman</li>
+                        <li>Kids</li>
+                        <li>Trendy</li>
+                    </ul>
+                </div>
+                <div class="radius">
+                    <div class="icon"><i class="fa-solid fa-magnifying-glass"></i></div>
+                    <div class="icon"><i class="fa-solid fa-bag-shopping"></i></div>
+                </div>
+            </div>
+            <div class="isi-tengah">
+                <div class="isi-kiri">
+                    <h1>Fly Away With <br> Your Style.<i class="fa-solid fa-heart"></i></h1>
+                    <p>Fashion is what you're offered four times a year by designers.</p>
+                    <p>And style is what you choose.</p>
+                    <button>Shop Now</button>
+                </div>
+                <div class="isi-kanan">
+                    <div class="border">
+                        <img src="./img/ai3-removebg-preview.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="isi-tengah2">
+                <h1>Trending Now <img src="./img/emot.png" alt=""></h1>
+            </div>
+            <div class="isi-bawah">
+                <div class="left">
+                    <div>
+                        <p>Nike</p>
+                        <p>Air Force 1</p>
+                        <p>$145</p>
+                    </div>
+                    <img src="./img/sepatu.png" alt="">
+                </div>
+                <div class="mid">
+                    <div>
+                        <p>Man Brand</p>
+                        <p>Military Jacket</p>
+                        <p>$69</p>
+                    </div>
+                    <img src="./img/jaket.png" alt="">
+                </div>
+                <div class="right">
+                    <div>
+                        <p>Airbrush</p>
+                        <p>Multi Hit Hoodie</p>
+                        <p>$49</p>
+                    </div>
+                    <img src="./img/jaket2.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
